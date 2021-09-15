2c033aa3a686277677a1902da94957df4b8b7f6e
Tbot30 <78313959+Tbot30@users.noreply.github.com>
2021-09-15 21:42:59 +0200

    Not rlly 1 but its 1 on new pc :D

---
+++ 1.py
@@ -0,0 +1,39 @@
+#belgian number plates maker
+
+
+from typing import Awaitable
+
+
+letr1 = "A"
+letr2 = "A"
+letr3 = "A"
+letr = letr1, letr2, letr3
+
+num1 = 0
+num2 = 0
+num3 = 0
+nums = []
+
+
+if num2 == 9:
+    for i in range(num1 == 0, 10):
+        num1 = num1 + 1
+        nums.append(num1)
+
+elif num3 == 9:
+    for i in range(num2 == 0, 10):
+        num2 = num2 + 1
+        nums.append(num2)
+
+else:
+    for i in range(num3 == 0, 10):
+        num3 = num3 + 1
+        nums.append(num3)
+
+print(*nums, sep='')
+
+
+
+
+#print(*letr, sep='', end="-")
+#print(*nums, sep='')
